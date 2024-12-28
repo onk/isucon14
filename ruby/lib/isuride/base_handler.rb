a8b2b04ab43133b3ad68954e990328e8147edad4
Takafumi ONAKA <takafumi.onaka@gmail.com>
2024-12-28 14:36:28 +0900

    ログじゃま

--- ruby/lib/isuride/base_handler.rb
+++ ruby/lib/isuride/base_handler.rb
@@ -23,7 +23,7 @@ module Isuride
     INITIAL_FARE = 500
     FARE_PER_DISTANCE = 100
 
-    enable :logging
+    # enable :logging
     set :show_exceptions, :after_handler
 
     class HttpError < Sinatra::Error
