cb96793ff7757841053e35712764877eccbea2e9
Takafumi ONAKA <takafumi.onaka@gmail.com>
2024-12-28 17:18:57 +0900

    idenpotency_key？

--- ruby/lib/isuride/payment_gateway.rb
+++ ruby/lib/isuride/payment_gateway.rb
@@ -23,44 +23,26 @@ module Isuride
       # 失敗したらとりあえずリトライ
       # FIXME: 社内決済マイクロサービスのインフラに異常が発生していて、同時にたくさんリクエストすると変なことになる可能性あり
       retries = 0
-      begin
-        uri = URI.parse("#{@payment_gateway_url}/payments")
-        Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
-          req = Net::HTTP::Post.new(uri.request_uri)
-          req.body = b
-          req['Content-Type'] = 'application/json'
-          req['Authorization'] = "Bearer #{@token}"
+      idenpotency_key = ULID.generate
 
-          res = http.request(req)
+      uri = URI.parse("#{@payment_gateway_url}/payments")
+      Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+        req = Net::HTTP::Post.new(uri.request_uri)
+        req.body = b
+        req['Content-Type'] = 'application/json'
+        req['Authorization'] = "Bearer #{@token}"
+        req['Idempotency-Key'] = idenpotency_key
 
-          if res.code != '204'
-            # エラーが返ってきても成功している場合があるので、社内決済マイクロサービスに問い合わせ
-            get_req = Net::HTTP::Get.new(uri.request_uri)
-            get_req['Authorization'] = "Bearer #{@token}"
+        _request_post_payment(req)
+      end
+    end
 
-            get_res = http.request(get_req)
-
-            # GET /payments は障害と関係なく200が返るので、200以外は回復不能なエラーとする
-            if get_res.code != '200'
-              raise UnexpectedStatusCode.new("[GET /payments] unexpected status code (#{get_res.code})")
-            end
-            payments = JSON.parse(get_res.body, symbolize_names: true)
-
-            rides = retrieve_rides_order_by_created_at_asc.call
-
-            if rides.size != payments.size
-              raise ErroredUpstream.new("unexpected number of payments: #{rides.size} != #{payments.size}")
-            end
-          end
-        end
-      rescue => e
-        if retries < 5
-          retries += 1
-          sleep(0.1)
-          retry
-        else
-          raise e
-        end
+    def _request_post_payment(req, retry_count = 0)
+      raise if retry_count > 5
+      res = http.request(req)
+      if res.code != '204'
+        sleep 0.1
+       _request_post_payment(req, retry_count + 1)
       end
     end
   end
